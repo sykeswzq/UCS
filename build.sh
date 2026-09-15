98808642adda22218d62e58d32d1d8aa37fa8600
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-15 15:04:24 +0800

    v3.1.2 bump

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=3.1.1
+VER=3.1.2
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -156,8 +156,8 @@ cat > "$PLIST" << PLIST_EOF
   <string>com.sykes.ucs.schedule</string>
   <key>ProgramArguments</key>
   <array>
-    <string>/var/jb/Applications/UCS.app/HealthBoostApp</string>
-    <string>--auto-generate</string>
+    <string>/var/jb/usr/bin/uiopen</string>
+    <string>com.sykes.ucs.app</string>
   </array>
   <key>StartInterval</key>
   <integer>60</integer>
