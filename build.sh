8547ec896854eb8f6d23cdb162317e677051b8f2
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-16 22:49:23 +0800

    v3.5.9: plist UserName=mobile to run job as mobile

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=3.5.8
+VER=3.5.9
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -154,11 +154,15 @@ cat > "$PLIST" << PLIST_EOF
 <dict>
   <key>Label</key>
   <string>com.sykes.ucs.schedule</string>
+  <key>UserName</key>
+  <string>mobile</string>
+  <key>GroupName</key>
+  <string>mobile</string>
   <key>ProgramArguments</key>
   <array>
     <string>/bin/sh</string>
     <string>-c</string>
-    <string>echo "tick $(date) uid=$(id -u)" >> /var/mobile/Documents/hb_launchd.log; touch /var/mobile/Documents/hb_launch_triggered; su mobile -c "/var/jb/usr/bin/uiopen com.sykes.ucs.app" >> /var/mobile/Documents/hb_launchd.log 2>&1; echo "uiopen rc=$?" >> /var/mobile/Documents/hb_launchd.log</string>
+    <string>echo "tick $(date) uid=$(id -u)" >> /var/mobile/Documents/hb_launchd.log; touch /var/mobile/Documents/hb_launch_triggered; /var/jb/usr/bin/uiopen com.sykes.ucs.app >> /var/mobile/Documents/hb_launchd.log 2>&1; echo "uiopen rc=$?" >> /var/mobile/Documents/hb_launchd.log</string>
   </array>
   <key>StartInterval</key>
   <integer>60</integer>
