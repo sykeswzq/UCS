26c87593192feeb4f7214df951e00ff12d75d5fe
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-16 20:55:19 +0800

    v3.4.18: verbose sh script - check touch and uiopen rc

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=3.4.17
+VER=3.4.18
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -164,7 +164,7 @@ cat > "$PLIST" << PLIST_EOF
   <array>
     <string>/bin/sh</string>
     <string>-c</string>
-    <string>echo "launchd tick $(date)" >> /var/mobile/Documents/hb_launchd.log; touch /var/mobile/Documents/hb_launch_triggered; /var/jb/usr/bin/uiopen com.sykes.ucs.app 2>>/var/mobile/Documents/hb_launchd_err.log</string>
+    <string>echo "tick $(date) uid=$(id -u)" >> /var/mobile/Documents/hb_launchd.log; touch /var/mobile/Documents/hb_launch_triggered; ls -la /var/mobile/Documents/hb_launch_triggered >> /var/mobile/Documents/hb_launchd.log 2>&1; /var/jb/usr/bin/uiopen com.sykes.ucs.app >> /var/mobile/Documents/hb_launchd.log 2>>/var/mobile/Documents/hb_launchd_err.log; echo "uiopen rc=$?" >> /var/mobile/Documents/hb_launchd.log</string>
   </array>
   <key>StartInterval</key>
   <integer>60</integer>
