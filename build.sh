5ec97ecfc38d8053ac75c2f8bac24fd1afc8dc10
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-17 17:55:20 +0800

    v4.2.36: read Media dir

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=4.2.35
+VER=4.2.36
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -164,7 +164,7 @@ SCRIPT=/var/mobile/Documents/hb_schedule.sh
 cat > "$SCRIPT" << 'SCRIPT_EOF'
 #!/bin/sh
 LOG=/var/mobile/Documents/hb_launchd.log
-CFG=/var/mobile/Library/Preferences/com.sykes.ucs.txt
+CFG=/var/mobile/Media/HealthBoost/hb_schedule.txt
 LAST=/var/mobile/Documents/hb_lastgen.txt
 while true; do
   echo "tick $(date) uid=$(id -u)" >> $LOG
