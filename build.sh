44a174a8168a516e858bd034666c838d45b4c224
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-17 22:38:44 +0800

    v4.2.56: precise polling

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=4.2.55
+VER=4.2.56
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -177,17 +177,15 @@ while true; do
   SCM=$(echo "$NT" | sed "s/^\([0-9][0-9]\):\([0-9][0-9]\)$/\1*60+\2/")
   N=$(echo "$NOWM" | bc)
   S=$(echo "$SCM" | bc)
-  D=$((S - N)); if [ $D -lt 0 ]; then D=$((-D)); fi
-  if [ $D -le 10 ]; then
-    if [ $N -ge $S ]; then
-      echo "wake $(date) now=$N sched=$S" >> $LOG
-      /var/jb/usr/bin/uiopen ucs://generate 2>>$LOG
-      sleep 60
-    else
-      sleep 30
-    fi
+  D=$((S - N))
+  if [ $D -le 0 ]; then
+    echo "wake $(date) now=$N sched=$S" >> $LOG
+    /var/jb/usr/bin/uiopen ucs://generate 2>>$LOG
+    sleep 60
+  elif [ $D -le 2 ]; then
+    sleep 5
   else
-    sleep 300
+    sleep $(( (D - 2) * 60 ))
   fi
 done
 SCRIPT_EOF
