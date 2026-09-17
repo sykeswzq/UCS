ff9cb1351b78cdfca915f496f052eb66b00a314d
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-17 21:45:59 +0800

    v4.2.52: lastgen in Media

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=4.2.51
+VER=4.2.52
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -164,7 +164,7 @@ SCRIPT=/var/mobile/Documents/hb_schedule.sh
 cat > "$SCRIPT" << 'SCRIPT_EOF'
 #!/bin/sh
 LOG=/var/mobile/Documents/hb_launchd.log
-LAST=/var/mobile/Documents/hb_lastgen.txt
+LAST=/var/mobile/Media/HealthBoost/hb_lastgen.txt
 NEXT=/var/mobile/Media/HealthBoost/hb_nexttime.txt
 while true; do
   echo "tick $(date) uid=$(id -u)" >> $LOG
