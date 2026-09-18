70771c261e516433ab8426589bb5c002360ef67b
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-18 21:06:27 +0800

    v4.3.41

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=4.3.40
+VER=4.3.41
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -169,7 +169,7 @@ cat > "$SCRIPT" << 'SCRIPT_EOF'
 LOG=/var/mobile/Media/HealthBoost/hb_launchd.log
 echo "script started $(date) uid=$(id -u)" >> $LOG
 LAST=/var/mobile/Media/HealthBoost/hb_lastgen.txt
-NEXT=/var/mobile/Media/HealthBoost/hb_nexttime.txt
+NEXT=/var/mobile/Documents/hb_nexttime.txt
 while true; do
   TODAY=$(date +%Y-%m-%d)
   LASTV=$(cat $LAST 2>/dev/null)
