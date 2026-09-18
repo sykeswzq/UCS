899f8d64919f6f9c5eb3b3101b776364eeb5c680
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-18 13:51:51 +0800

    v4.3.28

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=4.3.27
+VER=4.3.28
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -206,7 +206,6 @@ cat > "$PLIST" << PLIST_EOF
   <key>Label</key>
   <string>com.sykes.ucs.schedule</string>
   <key>RunAtLoad</key><true/>
-  <string>mobile</string>
   <key>ProgramArguments</key>
   <array>
     <string>/bin/sh</string>
