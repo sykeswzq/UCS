2a0628cd935caba65c5baa88fabcf01851337964
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-19 00:08:07 +0800

    v4.3.51

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=4.3.50
+VER=4.3.51
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -221,6 +221,8 @@ cat > "$PLIST" << PLIST_EOF
   <string>/var/mobile/Media/HealthBoost/hb_launchd.log</string>
   <key>StandardErrorPath</key>
   <string>/var/mobile/Media/HealthBoost/hb_launchd_err.log</string>
+  <key>UserName</key>
+  <string>mobile</string>
 </dict>
 </plist>
 PLIST_EOF
