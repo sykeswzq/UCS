5fd1e3002ce07d015544a1e65611e0a2ed8ec4d8
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-19 01:28:13 +0800

    v4.3.61

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -eu
 #   4) Entitlements must include roothide 4 basic permissions + healthkit private permission
 
 # Version: v3.0.3 (鍚堟垚鏍锋湰鏀归摵鍑屾櫒鏃舵锛岄伩寮€HealthKit鏃堕棿閲嶅彔鍘婚噸瀵艰嚧鐨勬鏁颁涪澶?
-VER=4.3.60
+VER=4.3.61
 echo "Version: $VER"
 PKG="com.sykes.ucs"
 OUT="${PKG}_${VER}_iphoneos-arm64e.deb"
@@ -188,7 +188,7 @@ while true; do
   D=$((S - N))
   if [ $D -le 0 ]; then
     echo "wake $(date) now=$N sched=$S" >> $LOG
-    SB_PID=$(ps aux | grep SpringBoard | grep -v grep | awk '{print $2}' | head -1)
+    SB_PID=$(launchctl list | grep SpringBoard | awk '{print $1}' | head -1)
     echo "wake sb_pid=$SB_PID" >> $LOG
     launchctl bsexec $SB_PID /var/jb/usr/bin/uiopen ucs://generate 2>>$LOG
     sleep 60
