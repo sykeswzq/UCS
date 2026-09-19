d0218928227af7d1d821b767e805edc18377eba8
sykeswzq <97281429+sykeswzq@users.noreply.github.com>
2026-09-19 16:14:13 +0800

    v4.4.32: fix async VERIFY bug (writeVirtualStepSample is async, don't VERIFY before it finishes)

--- HealthBoostApp/HealthBoostApp.m
+++ HealthBoostApp/HealthBoostApp.m
@@ -1271,11 +1271,9 @@ static const NSTimeInterval kBatchIntervalSeconds = 60;  // 每批时间窗口 6
             [self _writeSteps:steps dist:distM flights:flights deviceRev:deviceRev index:index + 1];
         } else {
             HBLog(@"[UCS] all writes complete");
+            // v4.4.32: writeVirtualStepSample is async; it calls finishWithError when done.
+            // Don't call finishSuccess/verify here - that would VERIFY before virtual samples are written.
             [self writeVirtualStepSample:steps deviceRev:deviceRev];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self finishSuccess:deviceRev];
-                [self verifyStepsWritten];
-            });
         }
     }];
 }
